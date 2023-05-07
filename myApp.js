9c1cef18123edca873b2f658fabf11b33e1b9a0f
AbrahamCruz1 <jacgkof@gmail.com>
2023-05-06 20:16:35 -0600

    Enviamos ruta en get

--- myApp.js
+++ myApp.js
@@ -3,9 +3,10 @@ let app = express();
 
 console.log("Hello World");
 
+absolutePath = __dirname + '/views/index.html';
 
 app.get('/',function(req,res){
-    res.send("Hello Express");
+    res.sendFile(absolutePath);
     console.log("Hello Express")
 })
 
